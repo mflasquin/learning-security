f671864b2ce243375ed2c7cbaf841e5ebd45c022
Maxime Flasquin <maxime.flasquin@gmail.com>
2023-01-23 21:05:59 +0100

    Add subscription page

--- authentication/register.php
+++ authentication/register.php
@@ -1,22 +1,45 @@
 <html>
 <head>
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ma super app sécurisée - Inscription</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
+          crossorigin="anonymous">
 </head>
 <body>
-<form action="/signup" method="post">
-    <label for="username">Nom d'utilisateur :</label>
-    <input type="text" id="username" name="username">
-
-    <label for="email">Adresse email :</label>
-    <input type="email" id="email" name="email">
-
-    <label for="password">Mot de passe :</label>
-    <input type="password" id="password" name="password">
-
-    <label for="password-confirm">Confirmez le mot de passe :</label>
-    <input type="password" id="password-confirm" name="password-confirm">
-
-    <input type="submit" value="S'inscrire">
+<form action="/signup" method="post" class="needs-validation" novalidate>
+    <div class="form-group">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" class="form-control" id="username" name="username" required>
+        <div class="invalid-feedback">
+            S'il vous plaît entrez un nom d'utilisateur.
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="email">Adresse email :</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+        <div class="invalid-feedback">
+            S'il vous plaît entrez une adresse email valide.
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password">Mot de passe :</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+        <div class="invalid-feedback">
+            S'il vous plaît entrez un mot de passe.
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password-confirm">Confirmez le mot de passe :</label>
+        <input type="password" class="form-control" id="password-confirm" name="password-confirm" required>
+        <div class="invalid-feedback">
+            S'il vous plaît confirmez votre mot de passe.
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">S'inscrire</button>
 </form>
 </body>
 </html>
